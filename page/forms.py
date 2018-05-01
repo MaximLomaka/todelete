c72a769b3b3fbbf0d1325ebb59ef3cc743cf1edc
max <lomaka.maxim@gmail.com>
2018-05-01 18:37:02 +0300

    bh

--- page/forms.py
+++ page/forms.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class AddUserForm(forms.Form):
     first_name = forms.CharField(label="Your first name", max_length=50)
     last_name = forms.CharField(label="Your last name", max_length=50)
-    birth_date = forms.DateField(label="Your birthdate")
+    birth_date = forms.DateField(label="Your birth date")
     city = forms.CharField(label='Resident city', max_length=30)
     photo = forms.ImageField(required=False)
     bio = forms.CharField(max_length=100, required=False)
